d2b2476bcf3a234d69af94045664b5a1cd4ffea0
mohamedashraf001 <mohamedashraf11ma11@gmail.com>
2026-04-20 14:43:26 +0200

    first commit

--- controllers/main.py
+++ controllers/main.py
@@ -12,7 +12,6 @@ except ImportError:
 class ReportController(http.Controller):
     @http.route('/web/binary/download_report_excel', type='http', auth="user")
     def download_report_excel(self, ids, **kw):
-        # تنظيف الـ IDs المستلمة من الـ URL
         clean_ids = ids.replace('[', '').replace(']', '').split(',')
         report_ids = [int(i) for i in clean_ids if i.strip()]
         reports = request.env['report.advanced.data'].browse(report_ids)
@@ -21,10 +20,10 @@ class ReportController(http.Controller):
         workbook = xlsxwriter.Workbook(output, {'in_memory': True})
         sheet = workbook.add_worksheet('Report')
 
-        # التنسيقات
+
         bold = workbook.add_format({'bold': True, 'bg_color': '#EEEEEE', 'border': 1})
 
-        # العناوين
+
         headers = ['Title', 'Type', 'Date', 'Product', 'Qty', 'Price', 'Subtotal']
         for col, head in enumerate(headers):
             sheet.write(0, col, head, bold)
